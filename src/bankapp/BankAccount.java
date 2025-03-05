93a9b9c7c7ab36f2409a44fbf3b0e16d8bc5d015
Doug Shook <dshook@wustl.edu>
2025-03-05 05:23:57 -0600

    deposits are now functional!

---
+++ src/bankapp/BankAccount.java
@@ -0,0 +1,21 @@
+package bankapp;
+
+public class BankAccount {
+
+	private double balance;
+	
+	public BankAccount() {
+		this.balance = 0;
+	}
+	
+	public void deposit(double amount) {
+		if(amount < 0) {
+			throw new IllegalArgumentException();
+		}
+		this.balance += amount;
+	}
+	
+	public double getCurrentBalance() {
+		return this.balance;
+	}
+}
