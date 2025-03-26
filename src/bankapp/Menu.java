e07eea532262095175077772a641058365d141dc
XavierT-L <t.xavier@wustl.edu>
2025-03-25 20:02:53 -0500

    Deposits are now Functional

--- src/bankapp/Menu.java
+++ src/bankapp/Menu.java
@@ -1,5 +1,34 @@
 package bankapp;
 
+import java.util.Scanner;
+
 public class Menu {
+	
+	private BankAccount theAccount; 
+	
+	public Menu() {
+		theAccount = new BankAccount();
+	}
+	
+	//display methods don't need to be tested
+	public void displayOptions() {
+		System.out.println("Please enter an amount to be deposited");
+	}
+	
+	//methods that require user input (Scanner) don't need to be tested
+	public double getUserInput() {
+		Scanner keyboardInput = new Scanner(System.in);
+		double userInput = keyboardInput.nextDouble();
+		return userInput;
+	}
+	
+	//Can and should test methods the process user input
+	public void processUserInput(double amount) {
+		theAccount.deposit(amount);
+	}
+	
+	public BankAccount getAccount() {
+		return theAccount;
+	}
 
 }
--- src/tests/MenuTests.java
+++ src/tests/MenuTests.java
@@ -1,5 +1,25 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import bankapp.BankAccount;
+import bankapp.Menu;
+
 public class MenuTests {
+	
+	@Test
+	public void testUserDeposi() {
+		// 1. Create object being tested
+		Menu m = new Menu();
+		
+		//2. Call method being tested
+		m.processUserInput(25);
+		
+		//3. Use assertions to verify correctness
+		BankAccount account = m.getAccount();
+		assertEquals(account.getCurrentBalance(), 25.0, 0.005);
+	}
 
 }
