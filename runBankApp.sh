3217de4bd956c827c2581f286c20a03811d750e4
XavierT-L <156951924+XavierT-L@users.noreply.github.com>
2025-03-31 12:11:45 -0500

    Add Script to run menu

---
+++ runBankApp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the path to the source code directory
+PROJECT_DIR="./src"
+# Define the package and class name
+PACKAGE="bankapp"
+MAIN_CLASS="Menu"
+
+# Navigate to the source code directory
+cd $PROJECT_DIR
+
+# Compile the Java files
+javac $PACKAGE/*.java
+
+# Run the Menu class
+java $PACKAGE.$MAIN_CLASS
